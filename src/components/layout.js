3e00e87446f8ecc715fa700d427e0767009d7733
hiroaki <hark.arkw@gmail.com>
2020-06-08 12:16:20 +0900

    Layoutのexport変更

--- src/components/layout.js
+++ src/components/layout.js
@@ -5,14 +5,12 @@ import Footer from "../components/footer"
 
 import "./layout.css"
 
-export default function Layout({children}) {
-    return [
-        <div>
-            <Header />
-    
-            {children}
-    
-            <Footer />
-         </div>
-    ]
-}
+const Layout = ({ children }) => (
+    <div>
+      <Header />
+         { children }
+      <Footer />
+    </div>
+  )
+  
+  export default Layout
