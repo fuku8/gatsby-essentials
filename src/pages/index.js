c1f4fea81da5a1c49cd33a9bf91b10fea27d518e
hiroaki <hark.arkw@gmail.com>
2020-06-26 11:20:02 +0900

    blog list page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { graphql } from 'gatsby'
+import { graphql, Link } from 'gatsby'
 import Img from 'gatsby-image'
 
 import Layout from "../components/Layout"
@@ -58,6 +58,27 @@ export default function Home({ data }) {
           <Img fluid={data.berry.childImageSharp.fluid} alt="赤く熟したベリー" style={{ height: "100%" }} />
           </figure>
         </section>
+        <section>
+                <div className="container">
+                    <h2 className="sr-only">RECENT POSTS</h2>
+                    <div className="posts">
+                        {data.allContentfulBlogPost.edges.map(({ node }) => (
+                            <article className="post" key={node.id}>
+                                <Link to={`/blog/post/${node.slug}/`}>
+                                    <figure>
+                                        <Img
+                                            fluid={node.eyecatch.fluid}
+                                            alt={node.eyecatch.description}
+                                            style={{ height: "100%" }}
+                                        />
+                                    </figure>
+                                    <h3>{node.title}</h3>
+                                </Link>
+                            </article>
+                        ))}
+                    </div>
+                </div>
+            </section>
       </Layout>
   ]
 }
@@ -97,6 +118,25 @@ export const query = graphql`
         fluid(maxWidth: 1600) {
           ...GatsbyImageSharpFluid_withWebp
         }
+      }
+    }
+    allContentfulBlogPost(
+      sort: { order: DESC, fields: publishDate }
+      skip: 0
+      limit: 4
+      ) {
+      edges {
+          node {
+              title
+              id
+              slug
+              eyecatch {
+                  fluid(maxWidth: 573) {
+                    ...GatsbyContentfulFluid_withWebp
+                  }
+                  description
+              }
+          }
       }
     }
   }
